10a9d569a6e9f4f1c5b742fa8116cb3ec4a98253
Jacob Dennon <jdennon@ucsc.edu>
2025-04-04 17:12:26 -0700

    Safe commit before I attempt to remove build directory

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -547,3 +547,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/jdawg/Work/EUREKA/build/esp-idf/wifi_provisioning/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/jdawg/Work/EUREKA/build/esp-idf/main/cmake_install.cmake")
+endif()
+
