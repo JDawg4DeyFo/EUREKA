728580f90ba71b8e91ed696eb635b00ebe943a67
Jacob Dennon <jdennon@ucsc.edu>
2025-04-11 10:01:05 -0700

    Added simple while loop in test harness

--- main/SensorLibTest.c
+++ main/SensorLibTest.c
@@ -14,12 +14,21 @@
 #include "esp_log.h"
 static const char *TAG = "i2c-simple-example";
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static void delay_ms(int ms)
+{
+    vTaskDelay((ms) / portTICK_PERIOD_MS);
+}
+
 void app_main(void)
 {
 	esp_err_t ret;
 	short moisture;
 	float temp;
 	float humid;
+	int iteration_count = 0;
 
 	ret = I2C_Init();
 	if (ret != ESP_OK)
@@ -31,44 +40,54 @@ void app_main(void)
 	SensorsIDs_t sensors = SOIL | HUMID_TEMP;
 	SensorsIDs_t initialized = Sensors_Init(sensors);
 
-	if (initialized & SOIL)
+	while (1)
 	{
-		ret = Read_SoilMoisture(&moisture);
-		if (ret == ESP_OK)
+		iteration_count++;
+		if (initialized & SOIL)
 		{
-			ESP_LOGI(TAG, "Soil Moisture: %d", moisture);
+			ret = Read_SoilMoisture(&moisture);
+			if (ret == ESP_OK)
+			{
+				ESP_LOGI(TAG, "Soil Moisture: %d", moisture);
+			}
+			else
+			{
+				ESP_LOGW(TAG, "Failed to read soil moisture");
+			}
+
+			ret = Read_SoilTemperature(&temp);
+			if (ret == ESP_OK)
+			{
+				ESP_LOGI(TAG, "Soil Temperature: %.2f", temp);
+			}
+			else
+			{
+				ESP_LOGW(TAG, "Failed to read soil temperature");
+			}
 		}
 		else
 		{
-			ESP_LOGW(TAG, "Failed to read soil moisture");
+			ESP_LOGW(TAG, "soil sensor not initialized");
 		}
 
-		ret = Read_SoilTemperature(&temp);
-		if (ret == ESP_OK)
+		if (initialized & HUMID_TEMP)
 		{
-			ESP_LOGI(TAG, "Soil Temperature: %.2f", temp);
+			ret = Read_Air_HumidityTemperature(&temp, &humid);
+			if (ret)
+			{
+				ESP_LOGI(TAG, "temperature: %f, humidity: %f", temp, humid);
+			}
+			else
+			{
+				ESP_LOGW(TAG, "Failed to read humidity and temperature");
+			}
 		}
 		else
 		{
-			ESP_LOGW(TAG, "Failed to read soil temperature");
+			ESP_LOGW(TAG, "Humidity temp sensor not initialized");
 		}
-	} else 
-	{
-		ESP_LOGW(TAG, "soil sensor not initialized");
-	}
-
-	if (initialized & HUMID_TEMP)
-	{
-		ret = Read_Air_HumidityTemperature(&temp, &humid);
-		if (ret)
-		{
-			ESP_LOGI(TAG, "temperature: %f, humidity: %f", temp, humid);
-		}
-		else
-		{
-			ESP_LOGW(TAG, "Failed to read humidity and temperature");
-		}
-	} else {
-		ESP_LOGW(TAG, "Humidity temp sensor not initialized");
+		
+		delay_ms(500);
+		ESP_LOGW(TAG, "While loop iteration #: %d", iteration_count);
 	}
 }
