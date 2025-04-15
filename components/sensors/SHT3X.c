030f0ab8604215fee3afa9afac43702d86348f65
Jacob <jdennon@ucsc.edu>
2025-04-15 08:38:58 -0700

    Small commit

--- components/sensors/SHT3X.c
+++ components/sensors/SHT3X.c
@@ -20,9 +20,12 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-#include "sht3x.h"
+#include "SHT3X.h"
 #include "math.h"
 #include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 static const char *SHT3X_TAG = "sht3x";
 
@@ -70,8 +73,9 @@ uint8_t clear_status_register[]         = {0x30, 0x41};
 /*
 * Delay the execution in ms
 */
-void delay_ms(uint16_t delay) {
-    ets_delay_us(delay * 1000);
+static void delay_ms(int ms)
+{
+	vTaskDelay((ms) / portTICK_PERIOD_MS);
 }
 
 /*
@@ -105,24 +109,10 @@ uint8_t sht3x_generate_crc(const uint8_t* data, uint16_t count) {
 * Hence, it is required to wait the command execution time before issuing the read header.
 * Commands must not be sent while a previous command is being processed.
 */
-esp_err_t sht3x_send_command(uint8_t *command, i2c_master_dev_handle_t *Dev_Handle) {
-    esp_err_t err;
+esp_err_t sht3x_send_command(uint8_t *command, i2c_master_dev_handle_t Dev_Handle) {
+    esp_err_t err = ESP_OK;
 
-	err = ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, command, sizeof(command), I2C_MASTER_TIMEOUT_MS))
-
-    return err;
-}
-
-/*
-* Data sent to and received from the sensor consists of a sequence of 16-bit commands and/or 16-bit words
-* (each to be interpreted as unsigned integer, most significant byte transmitted first). Each data word is
-* immediately succeeded by an 8-bit CRC. In write direction it is mandatory to transmit the checksum.
-* In read direction it is up to the master to decide if it wants to process the checksum.
-*/
-esp_err_t sht3x_read(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t *Dev_Handle) {
-    esp_err_t err;
-
-	err = ESP_ERROR_CHECK(i2c_master_transmit_receive(Dev_Handle, hex_code, SHT3X_HEX_CODE_SIZE, measurements, size, I2C_MASTER_TIMEOUT_MS))
+	ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, command, sizeof(command), I2C_MASTER_TIMEOUT_MS));
 
     return err;
 }
@@ -133,11 +123,25 @@ esp_err_t sht3x_read(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2c
 * immediately succeeded by an 8-bit CRC. In write direction it is mandatory to transmit the checksum.
 * In read direction it is up to the master to decide if it wants to process the checksum.
 */
-esp_err_t sht3x_write(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t *Dev_Handle) {
-    esp_err_t err;
+esp_err_t sht3x_read(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t Dev_Handle) {
+    esp_err_t err = ESP_OK;
 
-	err = ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, hex_code, SHT3X_HEX_CODE_SIZE, I2C_MASTER_TIMEOUT_MS));
-	err |= ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, measurements, size, I2C_MASTER_TIMEOUT_MS));
+	ESP_ERROR_CHECK(i2c_master_transmit_receive(Dev_Handle, hex_code, SHT3X_HEX_CODE_SIZE, measurements, size, I2C_MASTER_TIMEOUT_MS));
+
+    return err;
+}
+
+/*
+* Data sent to and received from the sensor consists of a sequence of 16-bit commands and/or 16-bit words
+* (each to be interpreted as unsigned integer, most significant byte transmitted first). Each data word is
+* immediately succeeded by an 8-bit CRC. In write direction it is mandatory to transmit the checksum.
+* In read direction it is up to the master to decide if it wants to process the checksum.
+*/
+esp_err_t sht3x_write(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t Dev_Handle) {
+    esp_err_t err = ESP_OK;
+
+	ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, hex_code, SHT3X_HEX_CODE_SIZE, I2C_MASTER_TIMEOUT_MS));
+	ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, measurements, size, I2C_MASTER_TIMEOUT_MS));
 
     return err;
 }
@@ -148,44 +152,43 @@ esp_err_t sht3x_write(uint8_t *hex_code, uint8_t *measurements, uint8_t size, i2
 * Hence, it is required to wait the command execution time before issuing the read header.
 * Commands must not be sent while a previous command is being processed.
 */
-esp_err_t sht3x_send_command_and_fetch_result(uint8_t *command, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t *Dev_Handle) {
-    esp_err_t err;	
+esp_err_t sht3x_send_command_and_fetch_result(uint8_t *command, uint8_t *measurements, uint8_t size, i2c_master_dev_handle_t Dev_Handle) {
+    esp_err_t err = ESP_OK;	
 
-	err = ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, command, sizeof(command), I2C_MASTER_TIMEOUT_MS));
+	ESP_ERROR_CHECK(i2c_master_transmit(Dev_Handle, command, sizeof(command), I2C_MASTER_TIMEOUT_MS));
 
     delay_ms(1000);
 
-    err |= ESP_ERROR_CHECK(i2c_master_receive(Dev_Handle, measurements, size,I2C_MASTER_TIMEOUT_MS));
+    ESP_ERROR_CHECK(i2c_master_receive(Dev_Handle, measurements, size,I2C_MASTER_TIMEOUT_MS));
 
-    i2c_cmd_link_delete(cmd);
     return err;
 }
 
 /*
 * Start periodic measurement, signal update interval is 5 seconds.
 */
-esp_err_t sht3x_start_periodic_measurement() {
-    return sht3x_send_command(mps_4_repeatability_high);
+esp_err_t sht3x_start_periodic_measurement(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(mps_4_repeatability_high, Dev_Handle);
 }
 
 /*
 * Start periodic measurement with the accelerated response time (ART) feature.
 */
-esp_err_t sht3x_start_periodic_measurement_with_art() {
-    return sht3x_send_command(periodic_measurement_with_art);
+esp_err_t sht3x_start_periodic_measurement_with_art(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(periodic_measurement_with_art, Dev_Handle);
 }
 
 /*
 * Read sensor output. The measurement data can only be read out once per signal update interval
 * as the buffer is emptied upon read-out.
 */
-esp_err_t sht3x_read_measurement(sht3x_sensors_values_t *sensors_values) {
+esp_err_t sht3x_read_measurement(sht3x_sensors_values_t *sensors_values, i2c_master_dev_handle_t Dev_Handle) {
     measurements_t measurements = {
         .temperature = {{0x00, 0x00}, 0x00},
         .humidity = {{0x00, 0x00}, 0x00}
     };
 
-    esp_err_t err = sht3x_read(read_measurement, (uint8_t *) &measurements, sizeof(measurements));
+    esp_err_t err = sht3x_read(read_measurement, (uint8_t *) &measurements, sizeof(measurements), Dev_Handle);
 
     sensors_values->temperature = (175.0 * (((measurements.temperature.value.msb << 8) + measurements.temperature.value.lsb) / 65535.0)) - 45.0;
     sensors_values->humidity = 100.0 * ((measurements.humidity.value.msb << 8) + measurements.humidity.value.lsb) / 65535.0;
@@ -196,8 +199,8 @@ esp_err_t sht3x_read_measurement(sht3x_sensors_values_t *sensors_values) {
 * Stop periodic measurement to change the sensor configuration or to save power. Note that the sensor will only
 * respond to other commands after waiting 500 ms after issuing the stop_periodic_measurement command.
 */
-esp_err_t sht3x_stop_periodic_measurement() {
-    return sht3x_send_command(stop_periodic_measurement);
+esp_err_t sht3x_stop_periodic_measurement(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(stop_periodic_measurement, Dev_Handle);
 }
 
 /*
@@ -205,43 +208,43 @@ esp_err_t sht3x_stop_periodic_measurement() {
 * the power supply. When the system is in idle state the soft reset command can be sent to the SHT3x.
 * This triggers the sensor to reset its system controller and reloads calibration data from the memory.
 */
-esp_err_t sht3x_soft_reset() {
-    return sht3x_send_command(soft_reset);
+esp_err_t sht3x_soft_reset(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(soft_reset, Dev_Handle);
 }
 
 /*
 * Additionally, a reset of the sensor can also be generated using the "general call" mode according to
 * I2C-bus specification. This generates a reset which is functionally identical to using the nReset pin.
 */
-esp_err_t sht3x_general_call_reset() {
-    return sht3x_send_command(general_call_reset);
+esp_err_t sht3x_general_call_reset(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(general_call_reset, Dev_Handle);
 }
 
 /*
 * Switch the internal heater on.
 */
-esp_err_t sht3x_enable_heater() {
-    return sht3x_send_command(heater_enable);
+esp_err_t sht3x_enable_heater(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(heater_enable, Dev_Handle);
 }
 
 /*
 * Switch the internal heater off.
 */
-esp_err_t sht3x_disable_heater() {
-    return sht3x_send_command(heater_disable);
+esp_err_t sht3x_disable_heater(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(heater_disable, Dev_Handle);
 }
 
 /*
 * The status register contains information on the operational status of the heater, the alert mode
 * and on the execution status of the last command and the last write sequence.
 */
-esp_err_t sht3x_read_status_register(sht3x_sensor_value_t *sensors_value) {
+esp_err_t sht3x_read_status_register(sht3x_sensor_value_t *sensors_value, i2c_master_dev_handle_t Dev_Handle) {
     sht3x_sensor_value_t status_register = {
         .value = {0x00, 0x00},
         .crc = 0x00
     };
 
-    esp_err_t err = sht3x_read(read_status_register, (uint8_t *) &status_register, sizeof(status_register));
+    esp_err_t err = sht3x_read(read_status_register, (uint8_t *) &status_register, sizeof(status_register), Dev_Handle);
 
     if(err != ESP_OK) {
         ESP_LOGE(SHT3X_TAG, "read_status_register failed with status code: %s", esp_err_to_name(err));
@@ -253,6 +256,6 @@ esp_err_t sht3x_read_status_register(sht3x_sensor_value_t *sensors_value) {
 /*
 * Clear all flags in the status register.
 */
-esp_err_t sht3x_clear_status_register() {
-    return sht3x_send_command(clear_status_register);
+esp_err_t sht3x_clear_status_register(i2c_master_dev_handle_t Dev_Handle) {
+    return sht3x_send_command(clear_status_register, Dev_Handle);
 }
