05be5af6c8efe0f0fc0593142a8b4b054a368bb0
Jacob <jdennon@ucsc.edu>
2025-04-16 09:55:38 -0700

    added config for i2c

--- include/I2C.h
+++ include/I2C.h
@@ -17,13 +17,13 @@
  * PUBLIC #DEFINES                                                            *
  ******************************************************************************/
 // I2C DEFINES
-#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL // GPIO for clock. by  |idf.py menuconfig|
-#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA // GPIO for data line
-#define I2C_MASTER_NUM 0						// Which I2C Port we're using.
-#define I2C_MASTER_FREQ_HZ 100000				/*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0				/*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0				/*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS 1000
+#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL 		// GPIO for clock. by  |idf.py menuconfig|
+#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA 		// GPIO for data line
+#define I2C_MASTER_NUM 0								// Which I2C Port we're using.
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQ		/*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0						/*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0						/*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS CONFIG_I2C_MASTER_TIMEOUT
 
 #define WRITE_BIT I2C_MASTER_WRITE
 #define READ_BIT I2C_MASTER_READ
