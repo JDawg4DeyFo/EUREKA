0298d51b58c2fff0709dbe7dabf9037a8ef8d689
Jacob Dennon <jdennon@ucsc.edu>
2025-05-02 14:30:50 -0700

    working on clustermain

--- main/ClusterMain.c
+++ main/ClusterMain.c
@@ -9,6 +9,57 @@
  * 
  */
 
-int main(void) {
-    while(1);
+
+// Includes
+/******************************************************************************/
+#include <stddef.h>
+#include "esp_log.h"
+#include "esp_timer.h"
+
+#include "../include/LoRa_main.h"
+
+// Defines
+/******************************************************************************/
+
+// Datatypes
+/******************************************************************************/
+typedef enum {
+	NOTHING,
+	RAW_SENSOR_DATA,
+	PERIOD_UPDATE,
+	REQUEST_SENSOR_DATA,
+	PROCESSED_SENSOR_DATA,
+} PacketIDs_t
+
+// Typedefs
+/******************************************************************************/
+typedef struct {
+	unsigned char *Payload;
+	LORA_Packets *next_pkt;
+	LORA_Packets *last_pkt;
+	PacketIDs_t Pkt_Type;
+} LORA_Packets;
+
+// Variables
+/******************************************************************************/
+static const char *TAG = "ClusterMain.c";
+
+// Functions
+/******************************************************************************/
+// LORA ISR
+
+// function parser
+
+// main()
+/******************************************************************************/
+void app_main(void) {
+	// config and initialize
+		// init sensors
+		// init lora
+		// init power monitor
+
+	// main program
+	while (1) {
+		// parse new packet
+	}
 }
