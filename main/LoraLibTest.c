27417f38cb7954963190d532afa5e8fc6fcd5d71
Jacob Dennon <jdennon@ucsc.edu>
2025-05-05 13:06:06 -0700

    added in a few debug statements

--- main/LoraLibTest.c
+++ main/LoraLibTest.c
@@ -20,11 +20,15 @@
 static sx1262_handle_t LoRa;
 static uint8_t test_buf[5] = "Test";
 
+static const char *TAG = "LoraLibTest.c";
 
 void app_main(void){
+	ESP_LOGI(TAG, "Testing lora begin");
   sx1262_lora_begin(&LoRa);
+	ESP_LOGI(TAG, "Testing dio1 gpio init");
   sx1262_interface_dio1_gpio_init(&LoRa);
 
+	ESP_LOGI(TAG, "Testing shot receive mode");
   if (sx1262_lora_set_shot_receive_mode(&LoRa, tx_timeout)){
     sx1262_interface_debug_print("Set RX failed\n");
   
@@ -35,12 +39,15 @@ void app_main(void){
     sx1262_interface_delay_ms(tx_timeout);
     sx1262_interface_debug_print("Set RX works\n");
   }
+
+  ESP_LOGI(TAG, "Writing buffer");
   sx1262_write_buffer(&LoRa, 0x00, test_buf, sizeof(test_buf));
   /* set lora packet params */
   sx1262_set_lora_packet_params(&LoRa, SX1262_LORA_DEFAULT_PREAMBLE_LENGTH,SX1262_LORA_DEFAULT_HEADER, SX1262_LORA_DEFAULT_BUFFER_SIZE,
   SX1262_LORA_DEFAULT_CRC_TYPE, SX1262_LORA_DEFAULT_INVERT_IQ);
   sx1262_lora_set_send_mode(&LoRa);
 
+	ESP_LOGI(TAG, "Testing set_Tx");
   if (sx1262_set_tx(&LoRa, tx_timeout)){
     sx1262_interface_debug_print("Transmitting a packet failed\n");
   
@@ -53,6 +60,7 @@ void app_main(void){
 
   }
 
+	ESP_LOGI(TAG, "Testing continous wave");
   if(sx1262_set_tx_continuous_wave(&LoRa)){
     sx1262_interface_debug_print("TX Continuous fails\n");
     sx1262_interface_dio1_gpio_deinit();
@@ -64,4 +72,6 @@ void app_main(void){
     sx1262_lora_deinit(&LoRa);
     
   }
+
+	ESP_LOGI(TAG, "About to exit program");
 }
