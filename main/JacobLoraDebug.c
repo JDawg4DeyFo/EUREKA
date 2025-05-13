9cbf2ce65c91956344af0d72b7502ae6972ebe2b
Jacob Dennon <jdennon@ucsc.edu>
2025-05-13 15:50:29 -0700

    mirrored spi pins to different hardware

--- main/JacobLoraDebug.c
+++ main/JacobLoraDebug.c
@@ -12,12 +12,30 @@
 #include "../../include/LoRa_main.h"
 #include "driver/gpio.h"
 #include "../../include/driver_sx1262_cad_test.h"
+#include "soc/gpio_sig_map.h"
+#include "hal/gpio_types.h"
 
 #define tx_timeout 9000
+#define MIRROR_MISO 41
+#define MIRROR_MOSI 40
+
 
 static sx1262_handle_t LoRa;
 static uint8_t test_buf[5] = "Test";
 
+// Route SPI pins to other, accessible pins on the ESP32.
+void mirror_spi2();
+{
+	// Mirror MISO
+	gpio_reset_pin(MIRROR_MISO);
+	gpio_set_direction(MIRROR_MISO, GPIO_MODE_OUTPUT);
+	gpio_matrix_out(MIRROR_MISO, SPI2_Q_IDX, false, false);
+	
+	// Mirror MOSI
+	gpio_reset_pin(MIRROR_MOSI);
+	gpio_set_direction(MIRROR_MOSI, GPIO_MODE_OUTPUT);
+	gpio_matrix_out(MIRROR_MOSI, FSPI_D_OUT_IDX, false, false);
+}
 
 void app_main(void){
   sx1262_lora_begin(&LoRa);
