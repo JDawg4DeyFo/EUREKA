ab6903944b80e86fd4a15b51e8758f427eb49540
Jacob Dennon <jdennon@ucsc.edu>
2025-05-13 14:31:55 -0700

    added in my own test for lora

---
+++ main/JacobLoraDebug.c
@@ -0,0 +1,47 @@
+/**
+ * @file LoraLibTest.c
+ * @author Jacob Dennon (jdennon@ucsc.edu)
+ * @brief Test harness for the LoRa library
+ * @version 0.1
+ * @date 2025-04-16
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "../../include/LoRa_main.h"
+#include "driver/gpio.h"
+#include "../../include/driver_sx1262_cad_test.h"
+
+#define tx_timeout 9000
+
+static sx1262_handle_t LoRa;
+static uint8_t test_buf[5] = "Test";
+
+
+void app_main(void){
+  sx1262_lora_begin(&LoRa);
+  init_lora_task();
+  sx1262_interface_dio1_gpio_init(&LoRa);
+
+  
+  if (sx1262_lora_set_send_mode(&LoRa))
+  {
+    sx1262_interface_debug_print("Set send mode failed\n");
+    sx1262_interface_dio1_gpio_deinit();
+    sx1262_lora_deinit(&LoRa);
+  } 
+
+
+  if(sx1262_lora_send(&LoRa, test_buf, sizeof(test_buf))){
+    sx1262_interface_dio1_gpio_deinit();
+    sx1262_lora_deinit(&LoRa);
+  }else{
+    sx1262_interface_debug_print("Transmission is a success\n");
+    sx1262_interface_dio1_gpio_deinit();
+    sx1262_lora_deinit(&LoRa);
+  }
+  
+  while(1);
+  
+}
