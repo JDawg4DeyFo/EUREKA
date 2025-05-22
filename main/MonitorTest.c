c2dbfb85eae38ec5770f5a5cb6f030d8881d1a8f
Jacob Dennon <jdennon@ucsc.edu>
2025-05-21 18:50:12 -0700

    idk why this isn't working

--- main/MonitorTest.c
+++ main/MonitorTest.c
@@ -9,8 +9,7 @@
  * 
  */
 
-#include "../include/INA219.h"
-
+#include "../esp-lib/components/ina219/ina219.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -40,8 +39,8 @@ void app_main(void)
 	
 	// init
 	ina219_init(&Handle);
-	ina219_configure(&Handle, INA219_BUS_RANGE_32V, INA219_GAIN_0_125, 12INA219_RES_12BIT_1S, 12INA219_RES_12BIT_1S, INA219_MODE_CONT_SHUNT_BUS);
-	ina219_calibrate(&Handle, MAX_CURRENT, SHUNT_RESISTANCE);
+	ina219_configure(&Handle, INA219_BUS_RANGE_32V, INA219_GAIN_0_125, INA219_RES_12BIT_1S, INA219_RES_12BIT_1S, INA219_MODE_CONT_SHUNT_BUS);
+	ina219_calibrate(&Handle, SHUNT_RESISTANCE);
 
 
 	while (1)
@@ -51,13 +50,13 @@ void app_main(void)
 
 		// measure
 		ina219_get_current(&Handle, &Current);
-		ina219_get_bus_voltage(&Hanlde, &BusVoltage);
+		ina219_get_bus_voltage(&Handle, &BusVoltage);
 		ina219_get_shunt_voltage(&Handle, &ShuntVoltage);
 		ina219_get_current(&Handle, &Current);
 		ina219_get_power(&Handle, &Power);
 
 		// print out power
-		ESP_LOGI("Measurements....\n\tBus Voltage: %f\n\tShunt Voltage: %f\n\tCurrent:%f\n\tPower:%f", BusVoltage, ShuntVoltage, Current, Power);
+		ESP_LOGI(TAG, "Measurements....\n\tBus Voltage: %f\n\tShunt Voltage: %f\n\tCurrent:%f\n\tPower:%f", BusVoltage, ShuntVoltage, Current, Power);
 	}
 
 }
